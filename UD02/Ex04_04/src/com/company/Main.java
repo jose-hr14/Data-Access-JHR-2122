f3fb0d299a6a99a9e4587a933f6d2724a1e54078
jose-hr14 <71669454+jose-hr14@users.noreply.github.com>
2021-10-06 23:41:42 +0200

    bla bla

--- UD02/Ex04_04/src/com/company/Main.java
+++ UD02/Ex04_04/src/com/company/Main.java
@@ -3,9 +3,9 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	    Ex04_04_01.Ex01();
-        //Ex04_04_01.Ex04();
+	    //Ex04_04_01.Ex01();
         //Ex04_04_02.Ex02();
         //Ex04_04_03.Ex03();
+        //Ex04_04_01.Ex04();
     }
 }
