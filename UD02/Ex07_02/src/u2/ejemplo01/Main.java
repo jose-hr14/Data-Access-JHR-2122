d5077a8887d25925ba070e71dba73b564ee33472
jose-hr14 <71669454+jose-hr14@users.noreply.github.com>
2021-10-14 13:49:05 +0200

    07_02

--- UD02/Ex07_02/src/u2/ejemplo01/Main.java
+++ UD02/Ex07_02/src/u2/ejemplo01/Main.java
@@ -1,10 +1,24 @@
 package u2.ejemplo01;
 
+import org.xml.sax.SAXException;
+
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.parsers.SAXParser;
 import javax.xml.parsers.SAXParserFactory;
+import java.io.IOException;
 
 public class Main {
 
     public static void main(String[] args) {
-
+        try {
+            SAXParser saxParser = SAXParserFactory.newInstance().newSAXParser();
+            saxParser.parse("contact.xml", new XMLhander());
+        } catch (ParserConfigurationException e) {
+            e.printStackTrace();
+        } catch (SAXException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
--- UD02/Ex07_02/src/u2/ejemplo01/XMLhander.java
+++ UD02/Ex07_02/src/u2/ejemplo01/XMLhander.java
@@ -11,19 +11,41 @@ public class XMLhander extends DefaultHandler {
     {
         currentTag = qName;
 
-        if(currentTag.equals("contact"))
-        {
-            System.out.println("ID: " + attributes.getValue("id"));
-        }
     }
     public void characters(char characters[], int start, int length)
     {
         tagContent = new String(characters, start, length);
-        if(currentTag.equals("Name"))
-            System.out.println(tagContent);
     }
     public void endElement(String uri, String localName, String qName)
     {
-        
+        String father = "";
+        if(!currentTag.isBlank())
+        {
+            if(currentTag.equals("name"))
+            {
+                System.out.print("Full name: " + tagContent);
+            }
+            if(currentTag.equals("surname"))
+            {
+                System.out.println(" " + tagContent);
+            }
+            if(currentTag.equals("cell") && father.equals("phones"))
+            {
+
+            }
+            if(currentTag.equals("work") && father.equals("phones"))
+            {
+                System.out.println(tagContent);
+            }
+            if(currentTag.equals("home") && father.equals("phones"))
+            {
+                System.out.println(tagContent);
+            }
+            currentTag = "";
+        }
+        else
+        {
+            father = currentTag;
+        }
     }
 }
