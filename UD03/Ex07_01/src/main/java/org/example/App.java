3e4efac72976266ff35dbdc604a920dd05e7d807
jose-hr14 <71669454+jose-hr14@users.noreply.github.com>
2021-10-27 20:49:04 +0200

    a

--- UD03/Ex07_01/src/main/java/org/example/App.java
+++ UD03/Ex07_01/src/main/java/org/example/App.java
@@ -1,9 +1,24 @@
 package org.example;
 
-public class App 
+import java.sql.CallableStatement;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class App
 {
+    static final String url = "jdbc:postgresql://localhost:5432/employees";
+    static final String user = "postgres";
+    static final String password = "postgres";
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        try {
+            Connection connection = DriverManager.getConnection(url, user, password);
+
+            CallableStatement callableStatement = connection.prepareCall("call ex01('SALESMAN')");
+            callableStatement.executeQuery();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 }
