a59689f4afa0c07d2bb8cb688294ae3794a08299
jose-hr14 <71669454+jose-hr14@users.noreply.github.com>
2021-10-29 19:39:07 +0200

    commit

--- UD03/Ex07_01/src/main/java/org/Ex04/App.java
+++ UD03/Ex07_01/src/main/java/org/Ex04/App.java
@@ -1,6 +1,7 @@
 package org.Ex04;
 
 import java.sql.*;
+import java.util.Scanner;
 
 public class App {
     static final String url = "jdbc:postgresql://localhost:5432/employees";
@@ -8,9 +9,50 @@ public class App {
     static final String password = "postgres";
 
     public static void main(String[] args) {
-        Ex02("20");
-    }
+        Scanner keyboard = new Scanner(System.in);
+        String option = "";
+        while(!option.equals("0"))
+        {
+            System.out.print("Choose an option: ");
+            option = keyboard.nextLine();
+            switch (option)
+            {
+                case "1":
+                    Ex01Handler();
+                    break;
+                case "2":
+                    Ex02Handler();
+                    break;
+                case "3":
+                    break;
+                case "0":
+                    break;
+            }
+        }
 
+
+
+    }
+    public static void Ex01Handler(){
+        Scanner keyboard = new Scanner(System.in);
+        System.out.print("Type a job to show it's employees: ");
+        String job = new Scanner(System.in).nextLine();
+        Ex01(job);
+        System.out.println();
+    }
+    public static void Ex02Handler(){
+        try{
+            System.out.print("Type a department number to show it's employees: ");
+            String departmentNumberString = new Scanner(System.in).nextLine();
+            int departmentNumber = Integer.parseInt(departmentNumberString);
+            Ex02(departmentNumber);
+            System.out.println();
+        }
+        catch (NumberFormatException numberFormatException){
+            System.out.println("You didn't introduce a number");
+        }
+
+    }
     public static void Ex01(String job) {
         try {
             Connection connection = DriverManager.getConnection(url, user, password);
@@ -25,10 +67,10 @@ public class App {
         }
     }
 
-    public static void Ex02(String deptno) {
+    public static void Ex02(int deptno) {
         try {
             Connection connection = DriverManager.getConnection(url, user, password);
-            CallableStatement callableStatement = connection.prepareCall("SELECT ex02(" + "'" + deptno + "'" + ")");
+            CallableStatement callableStatement = connection.prepareCall("SELECT ex02(" + deptno + ")");
             ResultSet resultSet = callableStatement.executeQuery();
             while (resultSet.next()) {
                 String[] resultado = resultSet.getString(1).split(",");
