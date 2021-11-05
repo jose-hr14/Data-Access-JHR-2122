421789c293332d0317aa3abbe373a271c4806c47
jose-hr14 <71669454+jose-hr14@users.noreply.github.com>
2021-11-05 13:31:07 +0100

    a

--- UD03/FinalActivity/src/main/java/org/example/GUI.java
+++ UD03/FinalActivity/src/main/java/org/example/GUI.java
@@ -54,6 +54,7 @@ public class GUI {
                 Course course = (Course) comboBox2.getSelectedItem();
 
                 new Database().enrollStudent(student, course);
+                new Database().addScore(student);
                 
             }
         });
