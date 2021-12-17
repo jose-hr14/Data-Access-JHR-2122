b28321ce5047ce8f252e89e50d49239db467972f
jose-hr14 <71669454+jose-hr14@users.noreply.github.com>
2021-12-17 20:03:34 +0100

    Update Main.java

--- UD04/Ex03_04_01/src/com/company/Main.java
+++ UD04/Ex03_04_01/src/com/company/Main.java
@@ -5,6 +5,8 @@ import com.db4o.Db4o;
 import com.db4o.Db4oEmbedded;
 import com.db4o.ObjectContainer;
 
+import java.util.Scanner;
+
 //store en lugar de set, y retrieve en lugar del get
 public class Main {
     public static void main(String[] args) {
@@ -14,5 +16,17 @@ public class Main {
         db.store(new Author("1", "Smith", "German"));
         db.commit();
         db.close();
+    }
+
+    public static void createAuthor()
+    {
+        Scanner scanner = new Scanner(System.in);
+        Author author = new Author();
+        System.out.print("Introduce author code: ");
+        author.setCode(scanner.nextLine());
+        System.out.print("Introduce author name: ");
+        author.setName(scanner.nextLine());
+        System.out.println("Introduce author nationality");
+        
     }
 }
