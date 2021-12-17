9816a4257d0c9a66565676c2f7e71c8023666af8
jose-hr14 <71669454+jose-hr14@users.noreply.github.com>
2021-12-17 19:55:44 +0100

    a

--- UD04/Ex03_04_01/src/com/company/Main.java
+++ UD04/Ex03_04_01/src/com/company/Main.java
@@ -2,12 +2,17 @@ package com.company;
 
 
 import com.db4o.Db4o;
+import com.db4o.Db4oEmbedded;
 import com.db4o.ObjectContainer;
 
 //store en lugar de set, y retrieve en lugar del get
 public class Main {
     public static void main(String[] args) {
-        ObjectContainer db = Db4o.openFile("persona.dat");
+        ObjectContainer db = Db4oEmbedded.openFile(Db4oEmbedded.newConfiguration(), "museum.dat");
+        db.store(new Author("1", "Smith", "German"));
+        db.store(new Author("1", "Smith", "German"));
+        db.store(new Author("1", "Smith", "German"));
+        db.commit();
         db.close();
     }
 }
