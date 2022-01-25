f4e555afdbb9717dbf2dd1f876d5bd6a62e798f8
José Hernández <71669454+jose-hr14@users.noreply.github.com>
2022-01-25 20:44:19 +0100

    a

--- UD05/FinalActivity/src/main/java/com/jhr2122/unit5/finalactivity/HelloController.java
+++ UD05/FinalActivity/src/main/java/com/jhr2122/unit5/finalactivity/HelloController.java
@@ -1,55 +1,62 @@
 package com.jhr2122.unit5.finalactivity;
 
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.TextField;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.GridPane;
 
 public class HelloController {
-    public HelloController() {
-    }
-
-
-    @FXML
-    private GridPane upperPane;
-
-    @FXML
-    private ImageView iconUser;
-
-    @FXML
-    private GridPane bottomPanel;
-
-    @FXML
-    private ImageView iconLentBook;
 
     @FXML
     private AnchorPane anchorPane;
 
     @FXML
+    private GridPane bottomPanel;
+
+    @FXML
     private ImageView iconAdd;
-
-    @FXML
-    private ImageView iconExit;
-
-    @FXML
-    private ImageView iconRead;
 
     @FXML
     private ImageView iconBook;
 
     @FXML
+    private ImageView iconEdit;
+
+    @FXML
+    private ImageView iconExit;
+
+    @FXML
+    private ImageView iconLentBook;
+
+    @FXML
+    private ImageView iconRead;
+
+    @FXML
     private ImageView iconReturnBook;
 
     @FXML
-    private ImageView iconEdit;
+    private ImageView iconUser;
 
     @FXML
     private TextField label1;
 
     @FXML
-    void iconReadListener(ActionEvent event) {
-        label1.setText("JavaFX test");
+    private TextField label11;
+
+    @FXML
+    private GridPane paneBook;
+
+    @FXML
+    private GridPane paneUser;
+
+    @FXML
+    private GridPane upperPane;
+
+    @FXML
+    void iconReadListener(MouseEvent event) {
+
     }
+
 }
