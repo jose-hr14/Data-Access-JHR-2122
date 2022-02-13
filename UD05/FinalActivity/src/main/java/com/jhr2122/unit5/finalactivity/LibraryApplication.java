ab396af1e02817e853fbc63f504cb77b0d5b46c1
José Hernández <71669454+jose-hr14@users.noreply.github.com>
2022-02-13 18:29:02 +0100

    a

--- UD05/FinalActivity/src/main/java/com/jhr2122/unit5/finalactivity/LibraryApplication.java
+++ UD05/FinalActivity/src/main/java/com/jhr2122/unit5/finalactivity/LibraryApplication.java
@@ -9,12 +9,11 @@ import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 
 import java.io.IOException;
-//Hacer tabla de reservas, distinta a la de prestamos
 public class LibraryApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(LibraryApplication.class.getResource("library-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 550, 550);
+        Scene scene = new Scene(fxmlLoader.load(), 500, 500);
         stage.setTitle("Library Manager");
         stage.initStyle(StageStyle.UNDECORATED);
         stage.setScene(scene);
